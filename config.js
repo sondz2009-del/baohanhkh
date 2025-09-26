abea79a1a501d1650f5d3db4f003a1c99ac4d9fa
sondz2009-del <nguyenbaoson4102009@gmail.com>
2025-09-26 23:11:59 +0700

    Sua

--- config.js
+++ config.js
@@ -3,4 +3,7 @@
 // Leave empty string to use localStorage fallback.
 window.GAS_URL = "https://script.google.com/macros/s/AKfycbzX_jcJOSbjrq-lhn6iRnZEbOG4UenUuC49i9O8EGq5aJm47nYRNlOJXrXNF1pUgptZ/exec";
 
+// Google Identity Services OAuth Client ID (Web)
+// Fill with your value like: xxxxx-xxxxxxxx.apps.googleusercontent.com
+window.GIS_CLIENT_ID = "836374264211-8buh44v8346g7qlj7c85761f413a5v6o.apps.googleusercontent.com";
 
